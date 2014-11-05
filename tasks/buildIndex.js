7704217275ccda721a392ff5b5edadaac4b29919
Pavel Birukov <birukov.pavel@gmail.com>
2014-11-05 13:16:41 +0300

    parser

--- tasks/buildIndex.js
+++ tasks/buildIndex.js
@@ -1,39 +1,69 @@
 var fs = require('fs'),
-    dirname = './blocks/',
-    jsdom = require("jsdom");
+    dirName = './blocks/',
+    jsdom = require("jsdom"),
+    stringify = require('stringify-object'),
+    async = require('async');
 
-var getFirstCommentValueFromEl = function($el){
-    var value = $el.contents().filter(function() {
+var getFirstCommentValueFromEl = function ($el) {
+    var value = $el.contents().filter(function () {
         return this.nodeType === 8;
     }).get(0).nodeValue;
     return value ? value.trim() : '';
-}
+};
 
-var files = fs.readdirSync(dirname).map(function (path) {
-        jsdom.env({
-            file: dirname + path,
-            scripts: '../bower_components/jquery/dist/jquery.js',
-            done: function (errors, window) {
-                if (errors) {
-                    console.error(errors);
-                } else {
-                    var $ = window.$,
-                        $block = $("[_\\:block]"),
-                        blockDescription = getFirstCommentValueFromEl($block);
-                    if (!blockDescription) {
-                        return console.error('Блок не задокументирован');
-                    }
-                    console.log("Block name: ", $block.attr('_:block'), ' | ', blockDescription);
-                    $("[_\\:param]").each(function (i, e) {
-                            var paramDescription = getFirstCommentValueFromEl($(e));
-                            if (!paramDescription) {
-                                return console.error('Параметр не задокументирован');
-                            }
-                            console.log('Parameter: ' + $(e).attr('_:param'), ' | ', paramDescription);
-                        }
-                    )
+var getUTplFromEl = function ($el, $) {
+    $el.contents().filter(function () {
+        return this.nodeType === 8;
+    }).remove();
+    return $("<div />").append($el.clone()).html().replace(/\n/g, '').trim();
+};
+
+var index = {};
+async.each(fs.readdirSync(dirName), function (fileName, callback) {
+    var fileContent = fs.readFileSync(dirName + fileName, {encoding: 'utf8'});
+    jsdom.env(fileContent,
+        ['../bower_components/jquery/dist/jquery.js'],
+        function (errors, window) {
+            if (errors) {
+                callback(errors);
+            } else {
+                var $ = window.$,
+                    $block = $("[_\\:block]"),
+                    blockName = $block.attr('_:block'),
+                    blockDescription = getFirstCommentValueFromEl($block),
+                    blockIndex = {params: {}};
+                if (!blockDescription) {
+                    return callback('Блок не задокументирован');
                 }
+
+                $block.removeAttr('_:block');
+                blockIndex.description = blockDescription;
+
+                $("[_\\:param]").each(function (i, e) {
+                        var $e = $(e),
+                            paramName = $(e).attr('_:param'),
+                            paramDescription = getFirstCommentValueFromEl($e);
+                        if (!paramDescription) {
+                            return callback('Параметр не задокументирован');
+                        }
+
+                        $e.removeAttr('_:param').text('_:param:' + paramName);
+                        blockIndex.params[paramName] = {
+                            description: paramDescription
+                        }
+                    }
+                )
+                blockIndex.tpl = getUTplFromEl($block, $);
+                index[blockName] = blockIndex;
+                callback();
             }
         });
-    })
-    ;
+}, function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        fs.writeFileSync('./output/index.json', stringify(index, {singleQuotes: false}), {encoding: 'utf8'});
+        fs.writeFileSync('./output/index.jsonp', 'var INDEX = ' + stringify(index, {singleQuotes: false}));
+        console.log(stringify(index));
+    }
+});
